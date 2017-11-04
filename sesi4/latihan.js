fcd6e6098a1dfb8d6f76b392007c66a86a9a406b
yudiahmadh <yudiahmad@gmail>
2017-11-04 12:53:45 +0700

    sesi 3 dan 4

---
+++ sesi4/latihan.js
@@ -0,0 +1,53 @@
+/* alert('hello');
+
+
+var nama = "Yudi Ahmad";
+var webBasic = ['HTML', 'CSS', 'Javascript'];
+
+console.log(webBasic[0]);
+
+webBasic[1] = 'Cascading Style Sheet';
+console.log(webBasic[1]);
+
+
+var buah = ['jeruk', 'pisang', 'mangga', 'jambu', 'semangka', 'anggur', 'apel'];
+
+console.log(buah[0]);
+console.log(buah[4]);
+buah[4] = 'Semangka Australia';
+console.log(buah[4]); */
+
+/* var person = {
+    name     : "Rian Ariona",
+    age      : 18,
+    gadgets : {
+        hp : 'samsung',
+        jam : 'tissot'
+    },
+    hobby : ['tidur', 'ngoding']
+};
+
+
+console.log(person.name);
+console.log(person.gadgets.hp);
+console.log(person.hobby[1]); */
+
+/* var nilai = 4;
+if ( nilai >= 8 ) {
+    console.log("Selamat anda lulus, dengan nilai " +nilai);
+} else if ( nilai >= 6 ) {
+    console.log("Selamat anda lulus, dengan nilai pas-pasan");
+} else {
+    console.log("Belajar Lagi Ya");
+} */
+
+var jam = 20;
+if (jam >= 1 && jam <= 11) {
+    alert("Selamat pagi");
+} else if (jam >= 12 && jam <= 15) {
+    alert("Selamat siang");
+} else if (jam >= 16 && jam <= 18) {
+    alert("Selamat sore");
+} else {
+    alert("Selamat malam");
+}
